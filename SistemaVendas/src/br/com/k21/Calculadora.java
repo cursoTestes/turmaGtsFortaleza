18e68d8ab374247c01adf99f877cfd4bb057a78a
cfc <cfc@ufrj.br>
2014-03-29 13:58:45 -0300

    Commit inicial

---
+++ SistemaVendas/src/br/com/k21/Calculadora.java
@@ -0,0 +1,19 @@
+package br.com.k21;
+
+public class Calculadora {
+
+	public static double comissao(double valor_venda) {
+		if (valor_venda<=10000)
+		{
+			return valor_venda*0.05;
+		}
+		else
+		{
+			return valor_venda*0.06;
+		}
+			
+
+	}
+
+
+}
---
+++ SistemaVendas/testes/br/com/k21/TestCalculadoraComissao.java
@@ -0,0 +1,59 @@
+package br.com.k21;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestCalculadoraComissao {
+
+	@Test
+	public void calcula_comissao_uma_venda_de_cem_retornando_cinco_reais() {
+		double valor_venda = 100;
+		double resultado_esperado = 5;
+		
+		double resposta = Calculadora.comissao(valor_venda);
+		
+		assertEquals(resultado_esperado, resposta, 0.0000001);
+	}
+
+	@Test
+	public void calcula_comissao_uma_venda_dez_mil_retornando_quinhentos_reais() {
+		double valor_venda = 10000;
+		double resultado_esperado = 500;
+		
+		double resposta = Calculadora.comissao(valor_venda);
+		
+		assertEquals(resultado_esperado, resposta, 0.0000001);
+	}
+	
+	@Test
+	public void calcula_comissao_uma_venda_de_um_real_retornando_5_centavos() {
+		double valor_venda = 1;
+		double resultado_esperado = 0.05;
+		
+		double resposta = Calculadora.comissao(valor_venda);
+		
+		assertEquals(resultado_esperado, resposta, 0.0000001);
+
+	}
+	
+	@Test
+	public void calcula_comissao_uma_venda_cem_mil_retornando_seis_mil_reais() {
+		double valor_venda = 100000;
+		double resultado_esperado = 6000;
+		
+		double resposta = Calculadora.comissao(valor_venda);
+		
+		assertEquals(resultado_esperado, resposta, 0.0000001);
+	}
+	
+	@Test
+	public void calcula_comissao_uma_venda_duzento_mil_retornando_doze_mil_reais() {
+		double valor_venda = 200000;
+		double resultado_esperado = 12000;
+		
+		double resposta = Calculadora.comissao(valor_venda);
+		
+		assertEquals(resultado_esperado, resposta, 0.0000001);
+	}
+}
